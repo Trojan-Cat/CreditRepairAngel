e66f7e706b4a9cf2aee6e3114d6f71636de1ea55
Trojan-Cat <kajarrod@gmail.com>
2020-07-24 19:19:58 +1000

    Removed a " from gitignore

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import Layout from "../layout";
+import config from "../../data/SiteConfig";
+
+const Index = props => {
+  return (
+    <Layout>
+      <div>Contents</div>
+    </Layout>
+  );
+};
+
+export default Index;
