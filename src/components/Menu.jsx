44a2a17ea054f9320783adb3d456afe650d30bd6
Trojan-Cat <kajarrod@gmail.com>
2020-07-25 13:27:42 +1000

    added in the styled componets and menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "gatsby";
+import styled from "styled-compoents";
+
+const Menu = props => {
+  return (
+    <div>
+      <div>Menu options</div>
+    </div>
+  );
+};
+
+export default Menu;
