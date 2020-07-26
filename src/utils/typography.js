e7c210cb051d812828c5b15c407ae7355872e2e7
Trojan-Cat <kajarrod@gmail.com>
2020-07-26 10:22:14 +1000

    Added in typography.js and some content on the homepage

---
+++ src/utils/typography.js
@@ -0,0 +1,21 @@
+import Typography from "typography";
+
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.666,
+  headerFontFamily: [
+    "Roboto",
+    "Avenir Next",
+    "Helvetica Neue",
+    "Segoe UI",
+    "Helvetica",
+    "Arial",
+    "sans-serif"
+  ],
+  headerColor: "#393939",
+  bodyColor: "#222222",
+  bodyWeight: 300,
+
+  bodyFontFamily: ["Noto Sans JP", "Georgia", "serif"]
+});
+export default typography;
